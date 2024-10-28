f584904e687dc77bf9c0ccdf5be254818cfba078
tusaunyapat <141983396+tusaunyapat@users.noreply.github.com>
2024-10-28 14:38:46 +0700

    edit title and add resume

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [], // add domains if you need to serve images from an external source
+  },
+  // Any other custom settings can go here if needed
+};
 
 export default nextConfig;
--- src/app/component/About.tsx
+++ src/app/component/About.tsx
@@ -2,12 +2,18 @@
 import { motion, useAnimation } from "framer-motion";
 import { useEffect, useRef } from "react";
 // import { useState } from "react";
+import Link from "next/link";
 import Timeline from "./Timeline";
 export default function About() {
   const controls = useAnimation();
   const ref = useRef<HTMLDivElement | null>(null);
   // const [replay, setReplay] = useState(false);
 
+  const openPdf = () => {
+    // Replace with your PDF URL (can be a public file in /public or a direct URL)
+    const pdfUrl = "/resume28102024.pdf"; // place 'sample.pdf' in the public folder
+    window.open(pdfUrl, "_blank");
+  };
   useEffect(() => {
     const handleScroll = () => {
       if (ref.current) {
@@ -33,7 +39,7 @@ export default function About() {
   }, [controls]);
 
   return (
-    <div className="flex flex-col justify-start gap-10 px-2 w-full  items-center min-h-screen">
+    <div className="flex flex-col justify-start gap-10 px-2 w-full  items-center min-h-screen pt-16">
       <motion.div
         ref={ref}
         initial={{ opacity: 0, y: 20 }}
@@ -43,13 +49,19 @@ export default function About() {
       >
         <p className="text-5xl sm:text-7xl font-bold">About Me</p>
       </motion.div>
+
       <motion.div
         ref={ref}
         initial={{ opacity: 0, y: 20 }}
         animate={controls}
         transition={{ duration: 0.7, ease: "easeOut" }}
-        className={`text-md items-center md:items-start flex flex-row  text-justify md:w-9/12 justify-center`}
+        className={`text-md items-center md:items-end flex flex-col  text-justify md:w-9/12 justify-center`}
       >
+        <Link href="/assets/resume28102024.pdf" target="_blank">
+          <button className="px-4 py-2 text-white my-0 bg-slate-600 rounded hover:bg-white hover:text-slate-600 hover:border hover:border-slate-600">
+            see my resume
+          </button>
+        </Link>
         <p className="text-xl">
           Hi! I’m Tus, a Computer Engineering student with a keen interest in{" "}
           <span className="font-bold">web development</span> and{" "}
